1513de4975116a985d2d4e16bbd2c510ee7c5821
mynameiseva <38572698+mynameiseva@users.noreply.github.com>
2018-05-25 12:51:45 +0300

    basic routing, login, registration

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://name:name@ds237489.mlab.com:37489/connector',
+  secretOrKey: 'secret'
+} 
+
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//db config
+const db = require('./config/keys').mongoURI;
+
+mongoose.
+  connect(db)
+  .then(() => console.log('Mongodb was connected'))
+  .catch(e => console.log(`Problem with connection to mongodb ${e}`))
+//passport middleware
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
